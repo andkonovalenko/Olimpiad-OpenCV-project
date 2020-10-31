2174aae1a77972495e140b690bf85ac02d7b9c39
Alexandr <furgaylov5@gmail.com>
2020-10-31 11:04:16 +0200

    renamed folder.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import numpy as np
 import cv2
+import imutils
+
 
 def read_video_file():
-    filename = input("Enter video file name: ")
+    # filename = input("Enter video file name: ")
+    filename = "/resources/sample-1.mp4"
     return cv2.VideoCapture(filename)
 
 
@@ -11,7 +14,10 @@ def display_video_result():
     while capture.isOpened():
         ret, frame = capture.read()
 
-        cv2.imshow('frame', frame)
+        frame = imutils.resize(frame, width=1000)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        cv2.imshow('frame', hsv)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
